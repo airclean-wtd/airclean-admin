cc51a45ad8a3efb429c71060bc68e55f88b38861
dengxiangying <hehear@126.com>
2020-07-10 16:34:42 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# airclean_admin
-vue element project
+# airclean
+
+> 这是一个基于vue2.0 + elementUI 后台管理平台
+
+### 技术栈
+- 传说中的VUE全家桶(vue vue-router vuex)
+- axios
+- [Element UI](http://element-cn.eleme.io/#/zh-CN)
+- [Mock](http://mockjs.com)（生成随机数据，屌屌的）
+
+## 在线演示
+
+### 线上访问
+
+[在线演示戳我!戳我!](https://xiahuahua.github.io/vue-admin-demo)
+
+## 安装运行（Build Setup）
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+```
---
+++ src/mock/data/user.js
@@ -0,0 +1,25 @@
+import Mock from 'mockjs';
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'airclean',
+    password: '123456',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80',
+    name: 'airclean'
+  }
+];
+
+const Users = [];
+
+for (let i = 0; i < 86; i++) {
+  Users.push(Mock.mock({
+    id: Mock.Random.guid(),
+    name: Mock.Random.cname(),
+    addr: Mock.mock('@county(true)'),
+    'age|18-60': 1,
+    birth: Mock.Random.date(),
+    sex: Mock.Random.integer(0, 1)
+  }));
+}
+
+export { LoginUsers, Users };
